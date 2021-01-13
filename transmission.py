174caae810d62dfd1684773f926c4b45249d3c74
Clint McMahon <clint@nordicdev.io>
2021-01-13 14:06:55 -0600

    update

--- transmission.py
+++ transmission.py
@@ -7,17 +7,17 @@ from datetime import timedelta
 if __name__ == '__main__':
 
     #Get the date this is being run
-    now = datetime.datetime.now()+ timedelta(days=-1)
+    now = datetime.datetime.now()+ timedelta(days=-6)
 
     #Transmission starts at 10 PM Central Time
     start_date = now.strftime("%Y-%m-%dT22:00:00")
 
     #Transmission ends at 11 PM Central Time
     #Since we are only getting one hour worth of music, set the end date to the same as start date
-    end_date = start_date
-    
+    end_date = now.strftime("%Y-%m-%dT23:59:59")
+        
     #name = "Transmission On The Radio"
-    name = "Test Playlist"
+    name = "Transmission On The Radio"
     description = "DJ Jake Rudh's latest Transmission On The Radio set. Updated every Thursday night. Support the music @ TheCurrent.org/donate."
     args = [name,start_date, end_date, description]
     thecurrent.main(args)
